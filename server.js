2519338b0aff4b2cbbeb9937f4240e5ab7d78512
ethakotasivakumar <30645543+ethakotasivakumar@users.noreply.github.com>
2017-08-05 09:01:26 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,23 +9,17 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-app.get('/ui/artical-one', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'artical-one.html'));
-});
-
-app.get('/artical-two', function (req, res) {
-  res.send('Artical two is requested and will be provoded soon');
-});
-app.get('/artical-three', function (req, res) {
-  res.send('Artical three is requested and will be provoded soon');
-});
-
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
 
 app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
+});
+
+//Adding pages
+app.get('/ui/artical-one', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'artical-one.html'));
 });
 
 
