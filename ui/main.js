886db8f85431380eb7b9f1578174e5a68c655859
ethakotasivakumar <30645543+ethakotasivakumar@users.noreply.github.com>
2017-08-05 10:41:43 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,6 +3,13 @@ console.log('Loaded!');
 //moving the image
 
 var image=document.getElementById('logo');
+var marginleft;
+
+function moveRight(){
+    marginleft=marginleft+10;
+    image.marginleft=marginleft+'px';
+}
+
 image.onclick=function(){
     var interval=setInterval(moveRight,100)
  //   image.style.marginLeft='200px';
