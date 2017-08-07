cfc9de75d4612c44a53cc33db498c22bc542faa1
ethakotasivakumar <30645543+ethakotasivakumar@users.noreply.github.com>
2017-08-07 09:56:00 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -7,7 +7,7 @@ var marginleft=0;
 
 function moveRight(){
     marginleft=marginleft + 10;
-    img.style.marginleft=marginleft+'px';
+    img.style.marginleft=marginleft + 'px';
 }
 
 img.onclick=function(){
