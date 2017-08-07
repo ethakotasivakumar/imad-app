da676a7f99849af64109599ae2bbb5cad9b9047a
ethakotasivakumar <30645543+ethakotasivakumar@users.noreply.github.com>
2017-08-07 10:27:21 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -6,11 +6,11 @@ var img=document.getElementById('logo');
 var marginLeft=0;
 
 function moveRight(){
-    marginLeft=marginLeft + 5;
+    marginLeft=marginLeft + 1;
     img.style.marginLeft=marginLeft + 'px';
 }
 
 img.onclick=function(){
-    var interval=setInterval(moveRight, 500);
+    var interval=setInterval(moveRight, 5);
  //   image.style.marginLeft='200px';
 }
