337a6e1f4e3bb03c420835faa450bf474063597d
ethakotasivakumar <30645543+ethakotasivakumar@users.noreply.github.com>
2017-08-21 12:56:21 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
-
+var Pool=required('pg').Pool;
 var app = express();
 app.use(morgan('combined'));
 
