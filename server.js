00dc8a6e24f7e5faf9416ede921a364729cd2307
ethakotasivakumar <30645543+ethakotasivakumar@users.noreply.github.com>
2017-08-21 12:50:17 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,6 +9,9 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+app.get('/test-db',function(req,res){
+    //make a select request
+    });
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
