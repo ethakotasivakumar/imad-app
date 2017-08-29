2eb2129c966db2a11497ee137e534d9fdb222c3d
ethakotasivakumar <30645543+ethakotasivakumar@users.noreply.github.com>
2017-08-29 09:19:36 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ var config={
     database:'ethakotasivakumar',
     host:'db.imad.hasura-app.io',
     port:'5432',
-    password:ethakotasivakumar-34361
+    password:process.env.DB_password
 }
 
 app.use(morgan('combined'));
